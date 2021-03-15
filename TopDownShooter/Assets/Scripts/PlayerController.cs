355c4948f7b7a9d293508d9ebf2c148be9e79ae6
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-03-15 13:46:38 -0400

    add moveController

---
+++ TopDownShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField] float moveSpeed;
+    Vector2 moveInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+
+        moveInput.y = Input.GetAxis("Vertical");
+
+        transform.Translate(moveInput * Time.deltaTime * moveSpeed);
+    }
+}
